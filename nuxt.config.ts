08fdff41447ba172cd0ddf71b41c454ed0fe786f
muneo992 <info@gloriatrading.autos>
2025-03-20 11:49:58 +0900

    for project setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-03-16',
+  compatibilityDate: '2025-03-19',
   devtools: { enabled: true }
 })
 export default defineNuxtConfig({
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+      <h1>Welcome to My Nuxt App</h1>
+    </div>
+  </template>
+  
