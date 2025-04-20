eaf8fc82c569772b2b2c4e67dd25e3a483321941
muneo992 <info@gloriatrading.autos>
2025-04-20 22:41:12 +0900

    理由不明だが変更

--- plugins/auth0.js
+++ plugins/auth0.js
@@ -1,15 +1,24 @@
-import Auth0 from 'auth0-js';
+import createAuth0Client from '@auth0/auth0-spa-js';
+
+let auth0Client;
+
+const getAuth0Client = async () => {
+  if (!auth0Client) {
+    auth0Client = await createAuth0Client({
+      domain: 'dev-olkr47luvghfb4t5.us.auth0.com',
+      client_id: 'IHA75IDoCPFQcPHneVkn4viHW6eE1KWc',
+      redirect_uri: window.location.origin,
+    });
+  }
+  return auth0Client;
+};
 
 export default defineNuxtPlugin(() => {
-  const client = new Auth0.WebAuth({
-    domain: 'your-domain.auth0.com',
-    clientID: 'your-client-id',
-  });
-
-  // プラグインとして利用可能にする
   return {
     provide: {
-      auth0: client,
-    },
+      auth0: {
+        getClient: getAuth0Client
+      }
+    }
   };
 });
