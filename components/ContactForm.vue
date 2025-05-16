0fdc358b6a05f776641a7dc1d2ae77c9d882b6bd
muneo992 <info@gloriatrading.autos>
2025-05-16 04:42:25 +0000

    contactform

---
+++ components/ContactForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <form
+    name="contact"
+    method="POST"
+    data-netlify="true"
+    netlify-honeypot="bot-field"
+  >
+    <p>
+      <label>お名前: <input type="text" name="name" /></label>
+    </p>
+    <p>
+      <label>メールアドレス: <input type="email" name="email" /></label>
+    </p>
+    <p>
+      <label>メッセージ: <textarea name="message"></textarea></label>
+    </p>
+    <p>
+      <button type="submit">送信</button>
+    </p>
+  </form>
+</template>
