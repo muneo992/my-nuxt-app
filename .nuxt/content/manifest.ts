d0177f43ea72d04992176893477f4e5b77613116
muneo992 <info@gloriatrading.autos>
2025-05-18 01:32:44 +0000

    update

---
+++ .nuxt/content/manifest.ts
@@ -0,0 +1,33 @@
+export const checksums = {
+  "content": "v3.5.0--bm1pLTmy95H3e0cG4Z8fXiLQRh_GeWMfSd1MnTvX0jQ"
+}
+export const checksumsStructure = {
+  "content": "bgIYhpjRuV8zbHJE_CfelwKpJ_Td6YuGJwixiek8lmI"
+}
+
+export const tables = {
+  "content": "_content_content",
+  "info": "_content_info"
+}
+
+export default {
+  "content": {
+    "type": "page",
+    "fields": {
+      "id": "string",
+      "stem": "string",
+      "extension": "string",
+      "meta": "json",
+      "path": "string",
+      "title": "string",
+      "description": "string",
+      "seo": "json",
+      "body": "json",
+      "navigation": "json"
+    }
+  },
+  "info": {
+    "type": "data",
+    "fields": {}
+  }
+}
--- .nuxt/types/schema.d.ts
+++ .nuxt/types/schema.d.ts
@@ -2,6 +2,14 @@ import { NuxtModule, RuntimeConfig } from '@nuxt/schema'
 declare module '@nuxt/schema' {
   interface NuxtOptions {
     /**
+     * Configuration for `@nuxtjs/mdc`
+     */
+    ["mdc"]: typeof import("@nuxtjs/mdc").default extends NuxtModule<infer O> ? O : Record<string, any>
+    /**
+     * Configuration for `@nuxt/content`
+     */
+    ["content"]: typeof import("@nuxt/content").default extends NuxtModule<infer O> ? O : Record<string, any>
+    /**
      * Configuration for `@nuxt/devtools`
      */
     ["devtools"]: typeof import("@nuxt/devtools").default extends NuxtModule<infer O> ? O : Record<string, any>
@@ -12,6 +20,14 @@ declare module '@nuxt/schema' {
   }
   interface NuxtConfig {
     /**
+     * Configuration for `@nuxtjs/mdc`
+     */
+    ["mdc"]?: typeof import("@nuxtjs/mdc").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    /**
+     * Configuration for `@nuxt/content`
+     */
+    ["content"]?: typeof import("@nuxt/content").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    /**
      * Configuration for `@nuxt/devtools`
      */
     ["devtools"]?: typeof import("@nuxt/devtools").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
@@ -19,12 +35,22 @@ declare module '@nuxt/schema' {
      * Configuration for `@nuxt/telemetry`
      */
     ["telemetry"]?: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
-    modules?: (undefined | null | false | NuxtModule<any> | string | [NuxtModule | string, Record<string, any>] | ["@nuxt/devtools", Exclude<NuxtConfig["devtools"], boolean>] | ["@nuxt/telemetry", Exclude<NuxtConfig["telemetry"], boolean>])[],
+    modules?: (undefined | null | false | NuxtModule<any> | string | [NuxtModule | string, Record<string, any>] | ["@nuxtjs/mdc", Exclude<NuxtConfig["mdc"], boolean>] | ["@nuxt/content", Exclude<NuxtConfig["content"], boolean>] | ["@nuxt/devtools", Exclude<NuxtConfig["devtools"], boolean>] | ["@nuxt/telemetry", Exclude<NuxtConfig["telemetry"], boolean>])[],
   }
 }
 declare module 'nuxt/schema' {
   interface NuxtOptions {
     /**
+     * Configuration for `@nuxtjs/mdc`
+     * @see https://www.npmjs.com/package/@nuxtjs/mdc
+     */
+    ["mdc"]: typeof import("@nuxtjs/mdc").default extends NuxtModule<infer O> ? O : Record<string, any>
+    /**
+     * Configuration for `@nuxt/content`
+     * @see https://content.nuxt.com
+     */
+    ["content"]: typeof import("@nuxt/content").default extends NuxtModule<infer O> ? O : Record<string, any>
+    /**
      * Configuration for `@nuxt/devtools`
      * @see https://www.npmjs.com/package/@nuxt/devtools
      */
@@ -37,6 +63,16 @@ declare module 'nuxt/schema' {
   }
   interface NuxtConfig {
     /**
+     * Configuration for `@nuxtjs/mdc`
+     * @see https://www.npmjs.com/package/@nuxtjs/mdc
+     */
+    ["mdc"]?: typeof import("@nuxtjs/mdc").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    /**
+     * Configuration for `@nuxt/content`
+     * @see https://content.nuxt.com
+     */
+    ["content"]?: typeof import("@nuxt/content").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    /**
      * Configuration for `@nuxt/devtools`
      * @see https://www.npmjs.com/package/@nuxt/devtools
      */
@@ -46,7 +82,7 @@ declare module 'nuxt/schema' {
      * @see https://www.npmjs.com/package/@nuxt/telemetry
      */
     ["telemetry"]?: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
-    modules?: (undefined | null | false | NuxtModule<any> | string | [NuxtModule | string, Record<string, any>] | ["@nuxt/devtools", Exclude<NuxtConfig["devtools"], boolean>] | ["@nuxt/telemetry", Exclude<NuxtConfig["telemetry"], boolean>])[],
+    modules?: (undefined | null | false | NuxtModule<any> | string | [NuxtModule | string, Record<string, any>] | ["@nuxtjs/mdc", Exclude<NuxtConfig["mdc"], boolean>] | ["@nuxt/content", Exclude<NuxtConfig["content"], boolean>] | ["@nuxt/devtools", Exclude<NuxtConfig["devtools"], boolean>] | ["@nuxt/telemetry", Exclude<NuxtConfig["telemetry"], boolean>])[],
   }
   interface RuntimeConfig {
    app: {
@@ -68,9 +104,55 @@ declare module 'nuxt/schema' {
    nitro: {
       envPrefix: string,
    },
+
+   content: {
+      databaseVersion: string,
+
+      version: string,
+
+      database: {
+         type: string,
+
+         filename: string,
+      },
+
+      localDatabase: {
+         type: string,
+
+         filename: string,
+      },
+
+      integrityCheck: boolean,
+   },
   }
   interface PublicRuntimeConfig {
+   content: {
+      wsUrl: string,
+   },
 
+   mdc: {
+      components: {
+         prose: boolean,
+
+         map: any,
+      },
+
+      headings: {
+         anchorLinks: {
+            h1: boolean,
+
+            h2: boolean,
+
+            h3: boolean,
+
+            h4: boolean,
+
+            h5: boolean,
+
+            h6: boolean,
+         },
+      },
+   },
   }
 }
 declare module 'vue' {
